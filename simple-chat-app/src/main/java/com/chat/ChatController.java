6477f28353c5e7281f961ba963c7e3e0e6581766
arun <you@example.com>
2023-09-13 22:58:02 +0530

    commit

---
+++ simple-chat-app/src/main/java/com/chat/ChatController.java
@@ -0,0 +1,15 @@
+package com.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public OutputMessage send(ChatMessage message) {
+        return new OutputMessage(message.getContent(),message.getSender());
+    }
+}
