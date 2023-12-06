26ae5d8fdf216861bd95caa2ba6f6368333ec0a2
arun <you@example.com>
2023-12-07 00:11:53 +0530

    commit

--- simple-chat-app/src/main/java/com/chat/ChatController.java
+++ simple-chat-app/src/main/java/com/chat/ChatController.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public OutputMessage send(ChatMessage message) {
-        return new OutputMessage(message.getContent(),message.getSender());
-    }
+	@MessageMapping("/chat")
+	@SendTo("/topic/messages")
+	public OutputMessage send(ChatMessage message) {
+		return new OutputMessage(message.getContent(), message.getSender());
+	}
 }
--- simple-chat-app/src/main/java/com/chat/ChatMessage.java
+++ simple-chat-app/src/main/java/com/chat/ChatMessage.java
@@ -1,21 +1,23 @@
 package com.chat;
 
 public class ChatMessage {
-    private String content;
-    private String sender;
-    
-    
+	private String content;
+	private String sender;
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public String getSender() {
 		return sender;
 	}
+
 	public void setSender(String sender) {
 		this.sender = sender;
 	}
-    
+
 }
